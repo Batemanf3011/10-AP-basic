83818ddb937ab04cb69d01b3bd97e82038bcbec2
Batemanf3011 <batemanf3011@masseyhigh.school.nz>
2024-06-24 11:57:45 +1200

    Add files via upload

---
+++ P_03_imput_output.py
@@ -0,0 +1,14 @@
+username = input("what is your name? ")
+
+fav_num = int(input("what is your favourite number?"))
+
+Double = fav_num * 2
+Half = fav_num / 2
+square = fav_num * fav_num
+
+print(f"\nHi {username}, your favourite number is {fav_num} ")
+print(f"Double {fav_num} is {Double}")
+print(f"half {fav_num} is {Half}")
+print(f"{fav_num} squared is {square}")
+print()
+print(f"Have a nice day {username}")
